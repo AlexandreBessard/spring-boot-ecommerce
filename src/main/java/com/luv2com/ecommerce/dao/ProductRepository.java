ae8327109b29d2f7203970a07c834fac071abd1d
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-30 17:08:02 -0400

    Release 1.0

--- src/main/java/com/luv2com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2com/ecommerce/dao/ProductRepository.java
@@ -2,9 +2,9 @@ package com.luv2com.ecommerce.dao;
 
 import com.luv2com.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-                                                    //<Entity, Primary key>
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {  //<Entity, Primary key>
 
 }
